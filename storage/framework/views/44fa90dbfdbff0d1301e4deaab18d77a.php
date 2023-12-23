c01441bbc98635928801c652d0bab73064f4e60b
najla-alosime <67576405+najla-alosime@users.noreply.github.com>
2023-12-23 16:07:51 +0300

    Add files via upload

---
+++ storage/framework/views/44fa90dbfdbff0d1301e4deaab18d77a.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
+    rel="stylesheet">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo e(url('css/app.css')); ?>">
+    <title>Switter</title>
+</head>
+<body>
+<header>
+
+<div class="nav-link">
+  <a href="">
+    <i class="material-icons-outlined">flutter_dash</i>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">home</i>
+    <span>Home</span>
+  </a>
+
+
+  <a href="">
+    <i class="material-icons-outlined">tag</i>
+    <span>Explore</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">notifications</i>
+    <span>Notification</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">email</i>
+    <span>Massagess</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">bookmark_border</i>
+    <span>Bookmarks</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">list_alt</i>
+    <span>Lists</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">person</i>
+    <span>Profile</span>
+  </a>
+
+  <a href="">
+    <i class="material-icons-outlined">more</i>
+    <span>More</span>
+  </a>
+
+ 
+    <button class="tweet-button">
+
+    Sweet
+    </button>
+
+  </header>
+
+<main>
+
+    <div class="main-flex">
+
+      <div class="main-feed">
+      <div class="home-bar">
+        <span>Home</span>
+        <i class="material-icons-outlined">auto_awesome</i>
+      </div>
+      <div class="new-tweet">
+
+        <div class="new-tweet-detaild">
+<input type="text" placeholder="What's happening?">
+
+<div class="new-tweet-action">
+<ul>
+
+  <li>
+    <i class="material-icons-outlined">image</i>
+  </li>
+
+  <li>
+    <i class="material-icons-outlined">gif_box</i>
+  </li>
+
+  <li>
+    <i class="material-icons-outlined">poll</i>
+  </li>
+
+
+  <li>
+    <i class="material-icons-outlined">sentiment_satisfied</i>
+  </li>
+
+  <li>
+    <i class="material-icons-outlined">event</i>
+  </li>
+</ul>
+<button class="tweet-button">Sweet</button>
+</div>
+ </div>
+</div>
+
+
+
+
+      <div class="feed-tweet">
+        
+        <div class="feed-tweet-details">
+           <div class="sweetr-details">
+          <a href="" class="tweeter-name">
+                Najla Alosimee
+                <span class="tweeter-handle">
+                  @najla_alosime1
+                </span>
+
+               
+          </a>
+
+          <i class="material-icons-outlined">
+            more_horiz
+          </i>
+        </div>
+      
+            
+
+
+            <div class="sweet-text">
+
+              <p> i'm najla alosime from saudi </p>
+
+            </div>
+        
+
+            <div class="sweet-icons">
+
+              <i class="material-icons-outlined">
+                chat_bubble_outline
+              </i>
+              <i class="material-icons-outlined">
+                restart_alt
+              </i>
+              <i class="material-icons-outlined">
+                favorite_border
+              </i>
+              <i class="material-icons-outlined">
+               upload
+              </i>
+
+            </div>
+          </div>
+        </div>
+
+
+        
+        <div class="feed-tweet">
+        
+          <div class="feed-tweet-details">
+             <div class="sweetr-details">
+            <a href="" class="tweeter-name">
+                  Najla Alosimee
+                  <span class="tweeter-handle">
+                    @najla_alosime1
+                  </span>
+  
+                 
+            </a>
+  
+            <i class="material-icons-outlined">
+              more_horiz
+            </i>
+          </div>
+        
+              
+  
+  
+              <div class="sweet-text">
+  
+                <p> I graduated in 2022 from Imam Muhammad bin Saud Islamic University.My Major Computer Science </p>
+  
+              </div>
+          
+  
+              <div class="sweet-icons">
+  
+                <i class="material-icons-outlined">
+                  chat_bubble_outline
+                </i>
+                <i class="material-icons-outlined">
+                  restart_alt
+                </i>
+                <i class="material-icons-outlined">
+                  favorite_border
+                </i>
+                <i class="material-icons-outlined">
+                 upload
+                </i>
+  
+              </div>
+            </div>
+          </div>
+
+        <div class="feed-tweet">
+        
+          <div class="feed-tweet-details">
+             <div class="sweetr-details">
+            <a href="" class="tweeter-name">
+                  Najla Alosimee
+                  <span class="tweeter-handle">
+                    @najla_alosime1
+                  </span>
+  
+                 
+            </a>
+  
+            <i class="material-icons-outlined">
+              more_horiz
+            </i>
+          </div>
+        
+              
+  
+  
+              <div class="sweet-text">
+  
+                <p> What is Computer Science? 
+                  Computer science focuses on the development and testing of software and software systems.
+                   It involves working with mathematical models, data analysis and security, algo</p>
+  
+              </div>
+          
+  
+              <div class="sweet-icons">
+  
+                <i class="material-icons-outlined">
+                  chat_bubble_outline
+                </i>
+                <i class="material-icons-outlined">
+                  restart_alt
+                </i>
+                <i class="material-icons-outlined">
+                  favorite_border
+                </i>
+                <i class="material-icons-outlined">
+                 upload
+                </i>
+  
+              </div>
+            </div>
+          </div>
+
+     
+
+
+
+
+             
+
+                <div class="feed-tweet">
+        
+                  <div class="feed-tweet-details">
+                     <div class="sweetr-details">
+                    <a href="" class="tweeter-name">
+                          Najla Alosimee
+                          <span class="tweeter-handle">
+                            @najla_alosime1
+                          </span>
+          
+                         
+                    </a>
+          
+                    <i class="material-icons-outlined">
+                      more_horiz
+                    </i>
+                  </div>
+                
+                      
+          
+          
+                      <div class="sweet-text">
+          
+                        <p> My scientific specialty is computer science </p>
+          
+                      </div>
+                  
+          
+                      <div class="sweet-icons">
+          
+                        <i class="material-icons-outlined">
+                          chat_bubble_outline
+                        </i>
+                        <i class="material-icons-outlined">
+                          restart_alt
+                        </i>
+                        <i class="material-icons-outlined">
+                          favorite_border
+                        </i>
+                        <i class="material-icons-outlined">
+                         upload
+                        </i>
+          
+                      </div>
+                    </div>
+                  </div>
+
+
+
+      </div>
+      
+    </div>
+  </main>
+  
+  </main>
+
+</body>
+</html>
+
+
+<?php /**PATH C:\Users\HP\Sweetr\resources\views/sweetr.blade.php ENDPATH**/ ?>
